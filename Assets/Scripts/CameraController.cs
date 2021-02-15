3a472df7e8b5c7f488fc0ee2710ccb8e2992425d
UlrichV23 <ulrichv23@gmail.com>
2021-02-15 16:56:43 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    private Vector3 offset = new Vector3(10, 4.5f, -11);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make tha camera follow the player
+        transform.position = playerBody.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    public float movementSpeed;
+    private float horizontalMovement;
+    private float verticalMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+    //Player movement controller
+    private void PlayerMovement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
+        verticalMovement = Input.GetAxis("Vertical") * movementSpeed;
+        playerRb.velocity = new Vector3(horizontalMovement, 0, verticalMovement);
+        
+    }
+}
